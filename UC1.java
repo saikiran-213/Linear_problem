a0945f650272bf407d394097e3a8b91b0e710125
saikiran-213 <galalasaikiran03@gmal.com>
2023-04-10 00:18:49 +0530

    first commit

--- UC1.java
+++ UC1.java
@@ -4,67 +4,53 @@ import java.util.Scanner;
 
 public class UC1 {
 
-		/**
-		 * This method is created to calculate the length of two lines .
-		 * 
-		 * @param args
-		 */
+	/*
+	 * This method is created to calculate the length of two lines .
+	 * 
+	 * @param args
+	 */
 
-			public static void main(String[] args) {
-				// TODO Auto-generated method stub
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
 
-				/**
-				 * 1) Get the value from user for length of the two lines. 
-				 * 2)To find the length of the two lines
-				 */
-				 
-				System.out.println("Welcome to Line Comparsion Computation Program");
+			/*
+			 * 1) Get the value from user for length of the two lines. 
+			 */
+			 
+			System.out.println("Welcome to Line Comparsion Computation Program");
 
-				/*
-				 * 1) Get the value from user for length of the two lines.
-				 */
+			/*
+			 * 1) Get the value from user for length of the two lines.
+			 */
 
-				Scanner sc = new Scanner(System.in);
+			Scanner sc = new Scanner(System.in);
 
-				System.out.println("Please value of x1");
-				int x1 = sc.nextInt();
+			System.out.println("Please value of x1");
+			int x1 = sc.nextInt();
 
-				System.out.println("Please value of x2");
-				int x2 = sc.nextInt();
+			System.out.println("Please value of x2");
+			int x2 = sc.nextInt();
 
-				System.out.println("Please value of y1");
-				int y1 = sc.nextInt();
+			System.out.println("Please value of y1");
+			int y1 = sc.nextInt();
 
-				System.out.println("Please value of y2");
-				int y2 = sc.nextInt();
+			System.out.println("Please value of y2");
+			int y2 = sc.nextInt();
 
-				System.out.println("Please value of p1");
-				int p1 = sc.nextInt();
+			System.out.println("Please value of p1");
+			int p1 = sc.nextInt();
 
-				System.out.println("Please value of p2");
-				int p2 = sc.nextInt();
+			System.out.println("Please value of p2");
+			int p2 = sc.nextInt();
 
-				System.out.println("Please value of q1");
-				int q1 = sc.nextInt();
+			System.out.println("Please value of q1");
+			int q1 = sc.nextInt();
 
-				System.out.println("Please value of q2");
-				int q2 = sc.nextInt();
+			System.out.println("Please value of q2");
+			int q2 = sc.nextInt();
 
-				/**
-				 * 2)To find the length of the two lines.
-				 */
+			
+		}
 
-				double x = Math.pow((x2 - x1), 2);
-				double y = Math.pow((y2 - y1), 2);
-				int D1 = (int) Math.sqrt(x + y);
-				System.out.println("length of the line1:" + D1);
-
-				double p = Math.pow((p2 - p1), 2);
-				double q = Math.pow((q2 - q1), 2);
-				int D2 = (int) Math.sqrt(p + q);
-				System.out.println("length of the line1:" + D2);
-				sc.close();
-
-	}
 
 }

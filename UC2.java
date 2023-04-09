662879d7b0453f3efb11f26606e4490c4e17fe36
saikiran-213 <galalasaikiran03@gmal.com>
2023-04-10 00:21:07 +0530

    second commit

---
+++ UC2.java
@@ -0,0 +1,54 @@
+package linearcomparsion;
+
+		import java.util.Scanner;
+
+		public class UC2 {
+
+			/*
+			 * This method is created to calculate the length of two lines .
+			 * 
+			 * @param args
+			 */
+
+				public static void main(String[] args) {
+					// TODO Auto-generated method stub
+
+					/*
+					 * 1) Get the value from user for length of the two lines. 
+					 */
+					 
+					System.out.println("Welcome to Line Comparsion Computation Program");
+
+					/*
+					 * 1) Get the value from user for length of the two lines.
+					 */
+
+					Scanner sc = new Scanner(System.in);
+
+					System.out.println("Please value of x1");
+					int x1 = sc.nextInt();
+
+					System.out.println("Please value of x2");
+					int x2 = sc.nextInt();
+
+					System.out.println("Please value of y1");
+					int y1 = sc.nextInt();
+
+					System.out.println("Please value of y2");
+					int y2 = sc.nextInt();
+
+					System.out.println("Please value of p1");
+					int p1 = sc.nextInt();
+
+					System.out.println("Please value of p2");
+					int p2 = sc.nextInt();
+
+					System.out.println("Please value of q1");
+					int q1 = sc.nextInt();
+
+					System.out.println("Please value of q2");
+					int q2 = sc.nextInt();
+
+	}
+
+}
